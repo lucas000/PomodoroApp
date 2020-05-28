f1ce1d069b270e05f753ee50aa5ad8b778eeb8d4
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2020-05-28 11:34:52 -0300

    Create page Main
    Created page Main usign styled-components

---
+++ src/pages/MainTime/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Container, ContainerTime, Time} from './styles';
+
+const App = () => {
+  return (
+    <Container>
+      <ContainerTime>
+        <Time>25:00</Time>
+      </ContainerTime>
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/pages/MainTime/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background: #e71414;
+`;
+
+export const ContainerTime = styled.View`
+  flex: 1;
+  align-items: center;
+  margin-top: 200px;
+  background: #e71414;
+`;
+
+export const Time = styled.Text`
+  font-size: 60px;
+  color: #e8e4e1;
+`;
