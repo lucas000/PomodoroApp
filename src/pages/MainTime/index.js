97fa4ce18d973675bcaade5db8d9ea4013b3efed
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2020-06-15 18:29:48 -0300

    Add icons will be used in app

--- src/pages/MainTime/index.js
+++ src/pages/MainTime/index.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+Icon.loadFont();
 
-import {Container, ContainerTime, Time} from './styles';
+import {
+  Container,
+  ContainerTime,
+  Time,
+  ContainerCicleAndInterval,
+  TextInterval,
+  ContainerMinutes,
+  TextTime,
+  ContainerControlls,
+} from './styles';
 
 const App = () => {
   return (
     <Container>
       <ContainerTime>
-        <Time>25:00</Time>
+        <Time>22:00</Time>
       </ContainerTime>
+
+      <ContainerControlls>
+        <Icon name="play" size={48} color="#e8e4e1" />
+        <Icon name="refresh-cw" size={48} color="#e8e4e1" />
+      </ContainerControlls>
+
+      <ContainerCicleAndInterval>
+        <TextInterval>Ciclos</TextInterval>
+        <TextInterval>Intervalo</TextInterval>
+      </ContainerCicleAndInterval>
+
+      <ContainerMinutes>
+        <Icon name="arrow-up" size={30} color="#e8e4e1" />
+        <TextTime>24 min</TextTime>
+        <Icon name="arrow-down" size={30} color="#e8e4e1" />
+        <Icon name="arrow-up" size={30} color="#e8e4e1" />
+        <TextTime>5 min</TextTime>
+        <Icon name="arrow-down" size={30} color="#e8e4e1" />
+      </ContainerMinutes>
     </Container>
   );
 };
--- src/pages/MainTime/styles.js
+++ src/pages/MainTime/styles.js
@@ -2,19 +2,49 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   background: #e71414;
 `;
 
 export const ContainerTime = styled.View`
-  flex: 1;
-  align-items: center;
-  margin-top: 200px;
-  background: #e71414;
+  margin-top: 150px;
 `;
 
 export const Time = styled.Text`
   font-size: 60px;
+  color: #e8e4e1;
+`;
+
+export const ContainerControlls = styled.View`
+  flex-direction: row;
+  margin-top: 60px;
+  width: 150px;
+  justify-content: space-between;
+`;
+
+export const ContainerCicleAndInterval = styled.View`
+  flex-direction: row;
+  width: 250px;
+  margin-top: 220px;
+  justify-content: space-between;
+`;
+
+export const TextInterval = styled.Text`
+  font-size: 20px;
+  color: #e8e4e1;
+`;
+
+export const ContainerMinutes = styled.View`
+  flex-direction: row;
+  margin-top: 20px;
+  width: 300px;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export const TextTime = styled.Text`
+  font-size: 20px;
   color: #e8e4e1;
 `;
